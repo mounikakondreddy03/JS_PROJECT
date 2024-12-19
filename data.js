18efdab843d3252688993d8ffbe1c5da5ca990cb
unknown <mounikakondreddy67@gmail.com>
2024-12-19 20:59:09 +0530

    Botique Website created using HTML, CSS and JS

--- data.js
+++ data.js
@@ -551,3 +551,13 @@ input.addEventListener("keypress", (e) => {
         cardsDisplays(searchData)
     }
 })
+
+
+function toggleMenu() {
+    var menuItems = document.getElementById("MenuItems");
+    if (menuItems.style.display === "block") {
+        menuItems.style.display = "none";
+    } else {
+        menuItems.style.display = "block";
+    }
+}
